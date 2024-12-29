aebf8b28611d2c3a132a82c67663a277f93e8c59
Edwin Garcia <egarciavalle2014@gmail.com>
2024-12-29 02:44:04 -0600

    chore: learn about F# program entry point

---
+++ projects/FSharpBasics/ExplicitEntryPoint/Program.fs
@@ -0,0 +1,10 @@
+﻿// We can explicitly specify which function is the entry point
+// of the program by specifying the EntryPoint attribute.
+
+[<EntryPoint>]
+let main args =
+    // We recieve the arguments through the args parameter.
+    printfn "The args: %A" args
+
+    // Must return an integer.
+    0
