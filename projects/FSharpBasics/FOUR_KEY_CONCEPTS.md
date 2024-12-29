5d13651f0385068d0fdeaba7e6cd4c6e507c8196
Edwin Garcia <egarciavalle2014@gmail.com>
2024-12-29 02:31:39 -0600

    chore: learn about .NET top-level statements

---
+++ projects/FSharpBasics/FOUR_KEY_CONCEPTS.md
@@ -0,0 +1,6 @@
+# The four key concepts of FSharp
+
+1. Function oriented, not object oriented.
+2. Expressions rather than statements.
+3. Algebraic types, for domain models.
+4. Pattern matching.
---
+++ projects/FSharpBasics/ImplicitEntryPoint/Program.fs
@@ -0,0 +1,18 @@
+﻿// We don't define an entry point, so the compiler will generate one for us.
+// This is thanks to .NET top-level statements.
+
+printfn "Hello World from F#!"
+
+let createGreeting greeting name =
+    sprintf "%s, %s!" greeting name
+
+let sayGoodMorning = createGreeting ("Good morning")
+
+let ruthGreeting = sayGoodMorning "Ruth"
+
+printfn "%s" ruthGreeting
+
+// Get the command line arguments passed to the program.
+let args = System.Environment.GetCommandLineArgs()
+
+printfn "Command line arguments: %A" args
