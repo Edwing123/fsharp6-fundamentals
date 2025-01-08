ae06a0e7bc8b4b1aed239931b5af1485d8a3c013
Edwin Garcia <egarciavalle2014@gmail.com>
2025-01-08 14:15:12 -0600

    chore: experiment with generics

---
+++ projects/FSharpBasics/GenericsTest/Program.fs
@@ -0,0 +1,16 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+type Box<'a> = {
+ Value: 'a;
+}
+
+[<EntryPoint>]
+let main _ =
+ let box = { Value = 42 }
+
+ let stringBox = { Value = "Hello" }
+
+ printfn "%A" box
+ printfn "%A" stringBox
+ 0
+
