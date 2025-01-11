d72d0dcb19a84604ef9879e8f4335b95a05da3f4
Edwin Garcia <egarciavalle2014@gmail.com>
2025-01-10 20:18:25 -0600

    chore: create initial project code

---
+++ projects/TransactionProcessor/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+type LoggingLevel =
+    | Info
+    | Debug
+    | Error
+
+let log (level: LoggingLevel) (msg: string) = Console.WriteLine($"{level}: {msg}")
+
+let logInfo = log LoggingLevel.Info
+
+[<EntryPoint>]
+let main argv =
+    logInfo "transaction processor starting."
+    logInfo $"received args: {argv}"
+
+    Console.Write("actions: (d)eposit, (w)ithdra or (e)xit: ")
+
+    let action = Console.ReadLine()
+
+    logInfo $"selected action: {action}"
+
+    logInfo "transaction processor shutting down."
+    0
